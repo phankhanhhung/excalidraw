8926a5ceb8192696170367a8be3a09f7c308a229
Mark Tolmacs <mark@lazycat.hu>
2026-04-10 18:29:29 +0000

    fix: Various fixes
    Signed-off-by: Mark Tolmacs <mark@lazycat.hu>

--- packages/excalidraw/actions/actionFinalize.tsx
+++ packages/excalidraw/actions/actionFinalize.tsx
@@ -14,10 +14,7 @@ import {
   isLineElement,
 } from "@excalidraw/element";
 
-import {
-  invalidateFreeDrawIncrementalCanvas,
-  setFreeDrawPredictedPoint,
-} from "@excalidraw/element";
+import { invalidateFreeDrawIncrementalCanvas } from "@excalidraw/element";
 
 import {
   KEYS,
@@ -320,7 +317,6 @@ export const actionFinalize = register<FormData>({
 
     if (element && isFreeDrawElement(element)) {
       invalidateFreeDrawIncrementalCanvas(element);
-      setFreeDrawPredictedPoint(element.id, null);
     }
 
     return {
